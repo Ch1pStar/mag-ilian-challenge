fbd86ea751cdb89038065b87eaa6d478e6455442
Ilian Nikolov <i.nikolov@dopamine.bg>
2017-09-08 10:45:35 +0300

    * Various improvements: move code entry panel to right show/hide code panel style code panel with theme (changeable) fit canvas to screen submit code panel content on success with formspree

--- js/Game.js
+++ js/Game.js
@@ -1,4 +1,11 @@
 const ALLOWED_COMMANDS = ['x', 'y', 'rotation'];
+const START_SCRIPT =
+`// data = {rocket = {x, y, rotation}, target = {x, y}, gravity, flare, meteor}
+let x = data.rocket.x - 3;
+let y = data.rocket.y - 3;
+let rotation = data.rocket.rotation - 0.006;
+
+return {x, y, rotation};`;
 
 class Game {
 
@@ -7,23 +14,34 @@ class Game {
         this.height = window.innerHeight;
         this.config = {rocket: {x: 0, y: 0, rotation: 0}};
         this.rocket = null;
-        this.navigateInput = null;
 
         this.container = document.querySelector('#game-container');
-        this.navigateInput = this.container.querySelector('#navigate-input');
+        this.sidePanel = this.container.querySelector('#side-panel');
+        this.userCode = CodeMirror(this.container.querySelector('#code-container'), {
+            value: START_SCRIPT,
+            mode:  "javascript",
+            theme: "monokai",
+            lineWrapping: true,
+            lineNumbers: true,
+            autofocus: true,
+            cursorHeight: 1.2
+        });
+        this.userCode.execCommand('goDocEnd'); // move cursor to end
         this.launchButton = this.container.querySelector('#launch-button');
+        this.showPanelButton = this.container.querySelector('#show-side-panel');
 
-        this.renderer = PIXI.autoDetectRenderer(this.width, this.height, {
+        this.renderer = PIXI.autoDetectRenderer(1920, 1080, {
             backgroundColor: 0xEE3322,
         });
 
-        this.container.insertBefore(this.renderer.view, this.container.firstChild);
+        this.container.querySelector("#canvas-container").appendChild(this.renderer.view);
 
         this.stage = new PIXI.Container();
 
-        this.launchButton.addEventListener('click', this.init.bind(this));
+        this.launchButton.addEventListener('click', this.hideSidePanel.bind(this, this.init.bind(this)));
+        this.showPanelButton.addEventListener('click', this.showSidePanel.bind(this));
 
-        PIXI.animate.load(lib.stage, this.onLoaded.bind(this));
+        PIXI.animate.load(lib.stage, this.onLoaded.bind(this)); // TODO loader
     }
 
     onLoaded(inst) {
@@ -37,15 +55,45 @@ class Game {
 
         this.init();
 
-        this.update(performance.now);
+        this.renderer.render(this.stage);
+        //this.update(performance.now);
+
+        // some how-to-animation
+        setTimeout(() => { // do some button click animation instead
+            this.hideSidePanel(() => {
+                this.update(performance.now);
+            });
+        }, 1000);
+
+        //setTimeout(this.onWin.bind(this), 3000); // testing
     }
+
+    showSidePanel() {
+        TweenMax.to(this.sidePanel, 1.5, {right: 0});
+        TweenMax.to(this.showPanelButton, 0.5, {opacity: 0});
+        //this.showPanelButton.style.opacity = 0;
+    }
+
+    hideSidePanel(callback) {
+        TweenMax.to(this.sidePanel, 1.5, {
+            right: -460,
+            onComplete: callback,
+            onCompleteScope: this,
+        });
+        setTimeout(() => TweenMax.to(this.showPanelButton, 0.5, {opacity: 1}), 1000);
+    }
+
+    //onWin() {
+    //    console.info("Mission complete!");
+    //    document.querySelector("#win-prompt").style.display = "block"
+    //    document.getElementsByName("submission")[0].value = this.userCode.value;
+    //}
 
     init() {
         this.rocket.x = this.config.rocket.x;
         this.rocket.y = this.config.rocket.y;
         this.rocket.rotation = this.config.rocket.rotation;
-        this.navigate = new Function('data', this.navigateInput.value); // TODO catch user input errors
-
+        this.navigate = new Function('data', this.userCode.getValue()); // TODO catch user input errors
     }
 
     update(t) {
@@ -70,6 +118,7 @@ class Game {
         requestAnimationFrame(this.update.bind(this));
     }
 
+    /** define rocket movement */
     navigate(data) {
         // user input evaled here
     }
