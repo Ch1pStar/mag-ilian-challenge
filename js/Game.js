53c1d8cf5ed31835f4115cabc4f7cfcead43bb9f
1le <ilian.nikolov@gmail.com>
2017-09-20 08:26:26 +0300

    manual transfer and cleanup from WIP branch

--- js/Game.js
+++ js/Game.js
@@ -1,4 +1,4 @@
-const ALLOWED_COMMANDS = ['x', 'y', 'rotation'];
+const ALLOWED_COMMANDS = ['x', 'y', 'rotation'];  // TODO - thrust
 
 function navigate(data) {
     // data = {rocket = {x, y, rotation}, target = {x, y}, gravity, flare, meteor}
@@ -14,6 +14,10 @@ src.shift();
 src.length -= 1;
 
 const START_SCRIPT = src.join('\n');
+const MAX_LANDING_VELOCITY = 15;
+const TOLERABLE_ANGLE_DEVIATION = Math.PI / 12; // 15 degrees
+const SIN_COS_TOLERATION = Math.sin( TOLERABLE_ANGLE_DEVIATION ); // 0.2588...
+const HALF_PI = Math.PI / 2;
 
 class Game {
 
@@ -23,6 +27,7 @@ class Game {
         this.config = {rocket: {x: 0, y: 0, rotation: 0}};
         this.zoom = 100;
         this.rocket = null;
+        this.target = null;
         this.planets = [];
         this.container = document.querySelector('#game-container');
         this.renderer = PIXI.autoDetectRenderer(1920, 1080, {
@@ -31,22 +36,29 @@ class Game {
         this.container.querySelector("#canvas-container").appendChild(this.renderer.view);
         this.stage = new PIXI.Container();
 
-        let prevTime = performance.now;
-        let time = 16.6;
+        this.prevTime = performance.now();
 
-        const update = this.update = (t) => {
-            time = Math.min(t - prevTime, 17);
-            prevTime = t;
-
-            this._doUpdate(time);
-            // this.world.step(time*.001);
-            this.world.step(0.016);
-            this.renderer.render(this.stage);
-
-            requestAnimationFrame(update);
-        };
+        this.tickBound = this.tick.bind(this);
 
         PIXI.animate.load(lib.stage, (inst) => this.onLoaded(inst)); // TODO loader
+
+        //document.addEventListener('mousemove', (e) => { // Debugging
+        // this.rocket.position[0] = e.clientX;
+        // this.rocket.position[1] = e.clientY;
+        //});
+    }
+
+    tick() {
+        let now = performance.now();
+        let delta = Math.min(now - this.prevTime, 17); // cap at 17 mls per frame
+        this.prevTime = now;
+
+        this.update(delta);
+        // this.world.step(time*.001);
+        this.world.step(0.016); // ?!? TODO
+        this.renderer.render(this.stage);
+
+        requestAnimationFrame(this.tickBound);
     }
 
     onLoaded(inst) {
@@ -67,11 +79,9 @@ class Game {
         // some how-to-animation
         // setTimeout(() => { // do some button click animation instead
             // this.hideSidePanel(() => {
-                this.update(performance.now);
+                this.tick();
             // });
         // }, 1000);
-
-        //setTimeout(this.onWin.bind(this), 3000); // testing
     }
 
     showSidePanel() {
@@ -89,11 +99,15 @@ class Game {
         setTimeout(() => TweenMax.to(this.showPanelButton, 0.5, {opacity: 1}), 1000);
     }
 
-    //onWin() {
-    //    console.info("Mission complete!");
-    //    document.querySelector("#win-prompt").style.display = "block"
-    //    document.getElementsByName("submission")[0].value = this.userCode.value;
-    //}
+    _gameOver(reason) {
+        alert("You dieded. " + reason); // TODO - fail screen
+    }
+
+    _onWin() {
+        console.info("Mission complete!");
+        document.querySelector("#win-prompt").style.display = "block";
+        document.getElementsByName("submission")[0].value = this.userCode.value; // TODO code input
+    }
 
     init() {
         this.rocket.position[0] = this.config.rocket.x;
@@ -108,7 +122,34 @@ class Game {
             broadphase: new p2.SAPBroadphase()
         });
 
+        world.on('impact', (evt) => {
+            if (evt.bodyA === this.target || evt.bodyB === this.target) {
+                if (evt.bodyB === this.rocket || evt.bodyA === this.rocket) {
+                    this._checkLanding();
+                }
+            }
+        });
+
         this.world = world;
+    }
+
+    _checkLanding() {
+        // check speed
+        let speed = Math.abs(this.rocket.velocity[0] * this.rocket.velocity[1]);
+        if (speed > MAX_LANDING_VELOCITY) return this._gameOver('Too fast.');
+
+        // check angle
+        var angleRadians = Math.atan2( // rocket entry angle
+            this.rocket.position[1] - this.target.position[1],
+            this.rocket.position[0] - this.target.position[0]
+        );
+        angleRadians += HALF_PI; // abcissa to ordinate based
+        if ((Math.abs( Math.sin(angleRadians) - Math.sin(this.rocket.rotation)) > SIN_COS_TOLERATION) ||
+            (Math.abs( Math.cos(angleRadians) - Math.cos(this.rocket.rotation)) > SIN_COS_TOLERATION)) { // OR tocket.angle?
+                return this._gameOver('Bad entry angle.');
+            }
+
+        this._onWin();
     }
 
     _initPlanets() {
@@ -117,9 +158,14 @@ class Game {
         const moon = animateStage.target;
 
         this._initPlanet(sun);
-        this._initPlanet(moon);
+        this.target = this._initPlanet(moon);
     }
 
+    /**
+     * @param {Pixi.Sprite} planet
+     * @returns {p2.Body}
+     * @private
+     */
     _initPlanet(planet) {
         const world = this.world;
         const body = new p2.Body({
@@ -156,6 +202,8 @@ class Game {
         });
 
         pMask.visible = false;
+
+        return body;
     }
 
     _initRocket() {
@@ -179,13 +227,27 @@ class Game {
         world.addBody(body);
 
 
-        // const g = new PIXI.Graphics();
-        // g.beginFill(0xff00ff);
-        // g.drawRect(-rocket.width/2, -rocket.height/2, rocket.width, rocket.height);
-        // g.endFill();
-        // g.alpha = 0.1;
-        // this.stage.addChild(g);
+         const g = new PIXI.Graphics();
+         g.beginFill(0xff00ff);
+         g.drawRect(-rocket.width/2, -rocket.height/2, rocket.width, rocket.height);
+         g.endFill();
+         g.alpha = 0.6;
+         this.stage.addChild(g);
 
+        window.a = g;
+
+        const s = PIXI.Sprite.fromImage('images/ar.png');
+        s.width = rocket.width;
+        s.height = rocket.height;
+
+        s.x = rocket.x;
+        s.y = rocket.y;
+        s.anchor.set(0.5)
+        s.alpha = .5;
+
+        window.s = s;
+
+        this.stage.addChild(s);
 
         body.position = new Proxy(body.position, {
             set: (target, property, value, receiver) => {
@@ -194,8 +256,13 @@ class Game {
                 rocket.y = target[1];
                 rocket.rotation = body.rotation + 1; // adjust for natural rocket texture rotation
 
-                // g.position.x = target[0];
-                // g.position.y = target[1];
+                g.position.x = target[0];
+                g.position.y = target[1];
+                g.rotation = body.rotation;
+
+                s.x = target[0];
+                s.y = target[1];
+                s.rotation = body.rotation; // adjust for natural rocket texture rotation
 
                 return true;
             }
@@ -209,6 +276,52 @@ class Game {
         this.config.rocket.rotation = rocket.rotation;
     }
 
+
+    _forwardVector(body) {
+        return [Math.sin(body.rotation), Math.cos(body.rotation)];
+    }
+
+    _thrust(mul = 10000) {
+        const r = this.rocket;
+        const f = p2.vec2.mul([], this._forwardVector(r), [mul, -mul]);
+
+        // return p2.vec2.mul(r.force, this._forwardVector(r), [mul, -mul]);
+        return r.applyForce(f);
+    }
+
+    go() {
+        this._onCommand('KeyW');
+    }
+
+    break() {
+        this._onCommand('KeyS');
+    }
+
+    left() {
+        this._onCommand('KeyA');
+    }
+
+    right() {
+        this._onCommand('KeyD');
+    }
+
+    _onCommand(key) {
+        switch(key) {
+            case 'KeyW':
+                this._thrust(100);
+                break;
+            case 'KeyS':
+                this._thrust(-100);
+                break;
+            case 'KeyA':
+                this.rocket.rotation = this.rocket.rotation- .05%Math.PI/2 ;
+                break;
+            case 'KeyD':
+                this.rocket.rotation = this.rocket.rotation +.05%Math.PI/2;
+                break;
+        }
+    }
+
     _applyPlanetGravitation(planet) {
         const rocket = this.rocket;
         const diff = [];
@@ -220,9 +333,36 @@ class Game {
         rocket.velocity = p2.vec2.add(rocket.velocity, n, rocket.velocity);
     }
 
-    _doUpdate(t) {
+    update(delta) {
+        const r = this.rocket;
+        const t = this.target;
 
-        let navigationCommands = this.navigate({ // TODO catch user input errors
+        let rocketThrust = this.navigationOutput;
+        Object.assign(r, rocketThrust); // TODO update rocket dynamics
+
+        if(r.position[1] > 600){
+            this.go();
+        }else{
+            this.break();
+        }
+
+        // const a = Math.atan2(r.position[1] - t.position[1], r.position[0] - t.position[0]);
+        const ro = Math.abs(r.rotation % Math.PI/2);
+
+        if(ro < .5){
+            this.left();
+        }else{
+            this.right();
+        }
+
+         //this.rocket.rotation = a;
+
+        // this._applyPlanetGravitation(this.sun);
+        //this._applyPlanetGravitation(this.target);
+    }
+
+    get navigationOutput() {
+        let output = this.navigate({ // TODO catch user input errors
             rocket: {x: this.rocket.x, y: this.rocket.y, rotation: this.rocket.rotation},
             target: {x: 0, y: 0},
             flare: 0, // some horizontal force
@@ -231,15 +371,10 @@ class Game {
         });
 
         // filter non allowed commands
-        for (let key in navigationCommands) {
-            if (ALLOWED_COMMANDS.indexOf(key) === -1) delete navigationCommands[key];
+        for (let key in output) {
+            if (ALLOWED_COMMANDS.indexOf(key) === -1) delete output[key];
         }
-
-        Object.assign(this.rocket, navigationCommands);
-
-
-        this._applyPlanetGravitation(this.sun);
-        this._applyPlanetGravitation(this.target);
+        return output;
     }
 
     /** define rocket movement */
